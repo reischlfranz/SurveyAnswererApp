f1bc71197a26a8ac084c0ad89c6f91df122d796f
Reischl Franz <franzreischl@outlook.com>
2020-04-27 21:13:30 +0200

    Issue #18 - Increase scope of unit test to cover all (most) of the class properties

--- SurveyAnswererApp.Test/DummySurveyFactoryTest.cs
+++ SurveyAnswererApp.Test/DummySurveyFactoryTest.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SurveyAnswererApp.Models;
 using SurveyAnswererApp.Models.Survey;
+using Xamarin.Forms.PlatformConfiguration.GTKSpecific;
 
 namespace SurveyAnswererApp.Test {
   [TestClass]
@@ -13,20 +15,39 @@ namespace SurveyAnswererApp.Test {
     }
 
     [TestMethod]
-    public void SurveyHasProperties() {
+    public void DummySurveyHasProperties() {
       Assert.IsNotNull(_questionnaire.Description);
       Assert.IsNotNull(_questionnaire.Title);
       Assert.IsTrue(_questionnaire.Id > 0);
     }
 
     [TestMethod]
-    public void SurveyHasQuestions() {
+    public void DummySurveyHasQuestions() {
       Assert.IsTrue(_questionnaire.Questions.Count > 0);
       foreach (var question in _questionnaire.Questions) {
         Assert.IsNotNull(question.QuestionType);
         Assert.IsNotNull(question.QuestionText);
         Assert.IsTrue(question.Nr>0);
         Assert.IsTrue(question.Answers.Count>0);
+      }
+    }
+
+    [TestMethod]
+    public void IllegalSurveyArguments() {
+      Assert.ThrowsException<ArgumentException>(() => _questionnaire.Id = -1);
+      Assert.ThrowsException<ArgumentException>(() => _questionnaire.Id = long.MinValue);
+    }
+    
+    
+    [TestMethod]
+    public void ValidSurveyArguments(){
+      try {
+        _questionnaire.Id = 0;
+        _questionnaire.Id = 1;
+        _questionnaire.Id = long.MaxValue;
+      }
+      catch (Exception e) {
+        Assert.Fail("Asserted no Exception but got "+e.Message);
       }
     }
   }
