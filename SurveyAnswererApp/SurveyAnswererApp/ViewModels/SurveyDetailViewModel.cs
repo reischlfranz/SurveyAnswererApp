7e4fdc828d9ee087d66d7ffc111e91b9946a1e84
Reischl Franz <franzreischl@outlook.com>
2020-04-28 16:25:48 +0200

    Added ViewModels for SurveyList and SurveyDetail
    Related Work Items: #20

---
+++ SurveyAnswererApp/SurveyAnswererApp/ViewModels/SurveyDetailViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using SurveyAnswererApp.Models.Survey;
+using SurveyAnswererApp.Views;
+using Xamarin.Forms;
+
+namespace SurveyAnswererApp.ViewModels
+{
+  class SurveyDetailViewModel
+  {
+
+    public Questionnaire Questionnaire { get; set; }
+
+    public ICommand OpenSurveyCommand { get; set; }
+
+    public SurveyDetailViewModel()
+    {
+      OpenSurveyCommand = new Command((e) => ExecuteOpenSurveyCommand(e, EventArgs.Empty));
+    }
+
+    private void ExecuteOpenSurveyCommand(object sender, EventArgs args)
+    {
+      //throw new NotImplementedException();
+      App.Instance.MainPage = new NavigationPage(new QuestionnairePage());
+
+    }
+  }
+}
